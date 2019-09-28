be542cf1c83e47151fd0209b2797b0ef6ec6d454
Keegan Bowen <keegan.bowen@gmail.com>
2019-09-28 06:03:42 -0600

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -15,7 +15,6 @@ git clone https://github.com/jpegleg/revsec.git
 git clone https://github.com/jpegleg/five-daemon-mgmt.git
 git clone https://github.com/jpegleg/sec-enforce.git
 
-
 # SEC section
 
 git clone https://github.com/simple-evcorr/sec.git
@@ -53,8 +52,36 @@ git clone https://github.com/tensorflow/tensorflow
 
 git clone https://github.com/josephmisiti/awesome-machine-learning.git
 git clone https://github.com/rasbt/python-machine-learning-book-2nd-edition.git
+git clone https://github.com/wepe/MachineLearning.git
+git clone https://github.com/udacity/machine-learning.git
+
+# "Blue Team" misc
+
+git clone https://github.com/tony-landis/PonyExpress.git
+git clone https://github.com/pfsense/pfsense.git
+git clone https://github.com/cloudflare/bpftools.git
+git clone https://github.com/rook2pawn/node-ddos
+git clone https://github.com/fcaviggia/hardened-centos7-kickstart.git
+git clone https://github.com/naingyeminn/CentOS7_Lockdown.git
+git clone https://github.com/konstruktoid/ansible-role-hardening.git
+git clone https://github.com/dev-sec/ansible-nginx-hardening.git
+git clone https://github.com/andrewmclagan/nginx-hhvm-docker.git
+
+
+# Offensive security: denial of service
+
+git clone https://github.com/llaera/slowloris.pl.git
+git clone https://github.com/epsylon/ufonet.git
+git clone https://github.com/Souhardya/WarChild.git
+
 
 # Forensic section
 
+git clone https://github.com/volatilityfoundation/volatility.git
+git clone https://github.com/USArmyResearchLab/Dshell.git
 git clone https://github.com/ReFirmLabs/binwalk.git
+git clone https://github.com/Invoke-IR/PowerForensics.git
+git clone https://github.com/cugu/awesome-forensics
+
+
 
