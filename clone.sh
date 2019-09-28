df8588a1f12cfe172bf44ce7bdf3c7ea690fb3b7
Keegan Bowen <keegan.bowen@gmail.com>
2019-09-28 04:38:54 -0600

    Create clone.sh

---
+++ clone.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+# Clone a collection of repos.
+
+# jpegleg section
+
+git clone https://github.com/jpegleg/hist-event-reaction.git
+git clone https://github.com/jpegleg/readhist.pl.git
+git clone https://github.com/jpegleg/pattern-learner.git
+git clone https://github.com/jpegleg/tcp-bunny.git
+git clone https://github.com/jpegleg/pipe2sec.git
+git clone https://github.com/jpegleg/careful-squid.git
+git clone https://github.com/jpegleg/metarc.git
+git clone https://github.com/jpegleg/revsec.git
+git clone https://github.com/jpegleg/five-daemon-mgmt.git
+git clone https://github.com/jpegleg/sec-enforce.git
+
+
+# SEC section
+
+git clone https://github.com/simple-evcorr/sec.git
+
+
+# OpenSCAP section
+
+git clone https://github.com/OpenSCAP/openscap.git
+
+# Kubernetes section
+
+git clone https://github.com/kubernetes/kubernetes.git
+
+# Terraform section
+
+git clone https://github.com/hashicorp/terraform.git
+
+# Ansible section
+
+git clone https://github.com/ansible/ansible
+
+# Rust section
+
+git clone https://github.com/rust-lang/rust
+
+# Scapy section
+
+git clone https://github.com/secdev/scapy
+
+# ML main section
+
+git clone https://github.com/tensorflow/tensorflow
+
+# ML learnging section
+
+git clone https://github.com/josephmisiti/awesome-machine-learning.git
+git clone https://github.com/rasbt/python-machine-learning-book-2nd-edition.git
+
+# Forensic section
+
+git clone https://github.com/ReFirmLabs/binwalk.git
+
