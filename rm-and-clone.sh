82909ddd8d594f4dcf9b5d6588820fafe3b6e9b4
Keegan Bowen <keegan.bowen@gmail.com>
2020-05-29 07:47:25 +0000

    Update and rename clone.sh to rm-and-clone.sh

---
+++ rm-and-clone.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+# Crudely blast away anything that was there!
+
+cd $1
+rm -rf ./*
+
+# Check out some of the jpegleg github code scraps:
+
+git clone https://github.com/jpegleg/deb-bast
+git clone https://github.com/jpegleg/tls-haproxy-config
+git clone https://github.com/jpegleg/ssh-key-hunter
+git clone https://github.com/jpegleg/zshrc
+git clone https://github.com/jpegleg/archive-spider
+git clone https://github.com/jpegleg/netstew
+git clone https://github.com/jpegleg/plumbus-freezer
+git clone https://github.com/jpegleg/bash-jwt
+git clone https://github.com/jpegleg/cryptoCore
+git clone https://github.com/jpegleg/iterblock
+git clone https://github.com/jpegleg/magic-spell
+git clone https://github.com/jpegleg/jpegleg-lib
+git clone https://github.com/jpegleg/jpegleg-vimrc
+git clone https://github.com/jpegleg/net-gargoyle
+git clone https://github.com/jpegleg/sec-alarm
+git clone https://github.com/jpegleg/open-aanpm
+git clone https://github.com/jpegleg/character-lib
+git clone https://github.com/jpegleg/spiral-arm
+git clone https://github.com/jpegleg/h2b2
+git clone https://github.com/jpegleg/forcematrix
+git clone https://github.com/jpegleg/mail-fixer
+git clone https://github.com/jpegleg/change-watcher
+git clone https://github.com/jpegleg/fe-html-template
+git clone https://github.com/jpegleg/flying-m
+git clone https://github.com/jpegleg/heaven-pki
+git clone https://github.com/jpegleg/recurse-catalog
+git clone https://github.com/jpegleg/hist-event-reaction
+git clone https://github.com/jpegleg/readhist.pl
+git clone https://github.com/jpegleg/pattern-learner
+git clone https://github.com/jpegleg/tcp-bunny
+git clone https://github.com/jpegleg/pipe2sec
+git clone https://github.com/jpegleg/careful-squid
+git clone https://github.com/jpegleg/metarc
+git clone https://github.com/jpegleg/revsec
+git clone https://github.com/jpegleg/sec-enforce
+git clone https://github.com/jpegleg/time-sort-tar
+git clone https://github.com/jpegleg/expiry-hunter
