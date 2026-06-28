66572e5d99ac59a3eb89b485f262cd4548870972
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2026-06-27 23:54:12 -0400

    Add clone command for tls-identity-manager repository

--- clone.sh
+++ clone.sh
@@ -146,3 +146,4 @@ git clone https://github.com/jpegleg/kiabluejay
 git clone https://github.com/jpegleg/vernal_edgewarden
 git clone https://github.com/jpegleg/llm_hunter
 git clone https://github.com/jpegleg/linux-disk-space-manager
+git clone https://github.com/jpegleg/tls-identity-manager
